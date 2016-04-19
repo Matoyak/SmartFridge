a0fb075db9e65ad764f6bb93b36ca1fa8980e1ef
Ola Siyanbola <oasiyanbola@gmail.com>
2016-04-19 10:58:41 -0600

    added validation

--- src/SmartFridge/Models/ApplicationUser.cs
+++ src/SmartFridge/Models/ApplicationUser.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace SmartFridge.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Required(ErrorMessage = "First Name cannot be empty")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name cannot be empty")]
         public string LastName { get; set; }
+
+        public string  FullName { 
+            get { return FirstName + " " + LastName; }
+            
+        }
+
         public ICollection<Item> Items { get; set; }
     }
 }
