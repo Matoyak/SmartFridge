746372e206f9ba8459872f1b63610fc4a1c94f75
Ola Siyanbola <oasiyanbola@gmail.com>
2016-04-19 09:55:59 -0600

    Renamed database properity category

--- src/SmartFridge/Services/Models/ItemDTO.cs
+++ src/SmartFridge/Services/Models/ItemDTO.cs
@@ -12,7 +12,7 @@ namespace SmartFridge.Services.Models {
 
         public int Barcode { get; set; }
 
-        public List<string> Categories { get; set; }
+        public ICollection<Category> Categories { get; set; }
 
         public string Name { get; set; }
     }
