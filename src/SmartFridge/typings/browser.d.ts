a780f8fb7cb88f9d02a617df901125bfc9c3159f
Mason Matlock <mwilliam09@gmail.com>
2016-04-27 14:01:46 -0500

    Fixed ngMoment, updated bower json

--- src/SmartFridge/typings/browser.d.ts
+++ src/SmartFridge/typings/browser.d.ts
@@ -3,4 +3,5 @@
 /// <reference path="browser/ambient/angular-ui-router/index.d.ts" />
 /// <reference path="browser/ambient/angular/index.d.ts" />
 /// <reference path="browser/ambient/jquery/index.d.ts" />
+/// <reference path="browser/ambient/moment-node/index.d.ts" />
 /// <reference path="browser/ambient/moment/index.d.ts" />
